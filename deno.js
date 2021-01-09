ec57ca5eadf13177ad649fbc8bf1fdc710804deb
Radu Rentea <radu.rentea@gmail.com>
2021-01-09 16:01:26 +0200

    Deno metrics

--- deno.js
+++ deno.js
@@ -3,3 +3,8 @@ const parent = Deno.args[1];
 if (food === 'love' && parent === 'radu') {
    console.log('🦕...Deno is born!')
 }
+
+setTimeout(() => {
+   console.log('check');
+},1000)
+console.table(Deno.metrics());
